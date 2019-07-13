e8ef7c170ed413aa318084b28800477f2883cabf
nihaal7 <40913961+nihaal7@users.noreply.github.com>
2019-07-13 17:39:56 +0530

    Add files via upload

---
+++ Chat/src/Main.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author nihaal
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        ChatForm form= new ChatForm();
+        form.setVisible(true);
+        // TODO code application logic here
+    }
+    
+}
